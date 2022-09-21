9ba97c5fbb2b3176809fe08b8b8964be94ed9d6b
Aimé <aime.lim-houn-tchen5@etu.univ-lorraine.fr>
2022-09-21 15:52:08 +0200

    avancement classe Formation

--- src/Formation.java
+++ src/Formation.java
@@ -1,3 +1,24 @@
+import java.util.HashMap;
+
 public class Formation {
-    test
+    public String identifiant;
+    public HashMap<String, Integer> mat;
+
+    public void addMat(String nom, int coeff){
+        if(!this.mat.containsKey(nom))
+          this.mat.put(nom, coeff);
+        throw new Error("a faire");
+    }
+
+    public void supMat(String nom){
+        if(this.mat.containsKey(nom))
+            this.mat.remove(nom);
+        throw new Error("a faire");
+    }
+
+    public float getCoeff(String nom){
+        if(this.mat.containsKey(nom))
+            return this.mat.get(nom);
+        throw new Error("Nom de matiere inexistant");
+    }
 }
