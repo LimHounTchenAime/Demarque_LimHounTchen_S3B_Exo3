ca256f44ad7b15855c71b1a0c26025a5f459a0a8
2Mo <demq.amaury@gmail.com>
2022-09-21 15:52:37 +0200

    Debut Etudiant + Identite + Diag classe

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,3 +1,21 @@
-public class Etudiant {
+import java.util.*;
 
+public class Etudiant {
+    private Identite id;
+    private Formation form;
+    private Map<String, Set<Float>> resultats;
+
+    public Etudiant(Identite i, Formation f){
+        this.id = i;
+        this.form = f;
+        this.resultats = new HashMap<String, Set<Float>>();
+//        for (this.form.mat.keySet():) {
+//
+//        }
+//        resultats.keySet()
+    }
+
+    public void addNote(String mat, float note){
+
+    }
 }
--- src/Identite.java
+++ src/Identite.java
@@ -1,2 +1,9 @@
 public class Identite {
+    private String NIP,nom,prenom;
+
+    public Identite(String nip, String n,String p){
+        this.NIP = nip;
+        this.nom = n;
+        this.prenom = p;
+    }
 }
