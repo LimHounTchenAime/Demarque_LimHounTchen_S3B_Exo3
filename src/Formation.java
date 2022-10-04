59f497ac27b608ce988951d5f96bd6ac01d26881
Aimé <aime.lim-houn-tchen5@etu.univ-lorraine.fr>
2022-10-04 14:46:38 +0200

    classe de test Formation

--- src/Formation.java
+++ src/Formation.java
@@ -1,9 +1,14 @@
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Formation {
     public String identifiant;
     public HashMap<String, Double> mat;
 
+    public Formation(){
+        this.mat=new HashMap<String, Double>();
+    }
     public void addMat(String nom, double coeff){
         if(!this.mat.containsKey(nom))
           this.mat.put(nom, coeff);
@@ -20,5 +25,14 @@ public class Formation {
         if(this.mat.containsKey(nom))
             return this.mat.get(nom);
         throw new Error("Nom de matiere inexistant");
+    }
+
+    public String toString(){
+        String r="";
+        Set<String> keys=mat.keySet();
+        for(String elem:keys){
+            r+=elem+" "+mat.get(elem)+"\n";
+        }
+        return r;
     }
 }
