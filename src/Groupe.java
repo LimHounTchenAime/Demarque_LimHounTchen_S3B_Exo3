b17718b2bc428373579ba7a2fc73dfe6560c1977
2Mo <demq.amaury@gmail.com>
2022-10-15 11:37:52 +0200

    modification Groupe

--- src/Groupe.java
+++ src/Groupe.java
@@ -35,14 +35,13 @@ public class Groupe {
         return moy/this.grp.size();
     }
 
-    public double calcMoyGen(){
+    public double calcMoyGen() {
         double moy = 0.0;
-        for(Etudiant e: this.grp.keySet()){
-            moy+=e.calcMoyGen();
+        for (Etudiant e : this.grp.keySet()) {
+            moy += e.calcMoyGen();
         }
-        return moy/this.grp.size();
+        return moy / this.grp.size();
     }
-    //System.out.println((((17.0*2.0)+(13.0*2.0)+(15.0*5.0)+(16.0*5.0))/(2.0+2.0+5.0+5.0)));
 
     public Map getGrp(){
         return this.grp;
